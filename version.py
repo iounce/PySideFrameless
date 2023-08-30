db5d635b632291519e2bc5321f52482b82e6c83a
iounce <iounce@foxmail.com>
2023-08-30 21:26:19 +0800

    update version

--- version.py
+++ version.py
@@ -2,9 +2,9 @@
 
 # version information of the application
 class AppVersion:
-    APP_VERSION = '1.0.0'
-    APP_BUILD = '97'
-    APP_BUILD_DATE = '2023-03-11'
+    APP_VERSION = '1.0.1'
+    APP_BUILD = 'b413dc7'
+    APP_BUILD_DATE = '2023-08-30'
 
     @staticmethod
     def get_version():
